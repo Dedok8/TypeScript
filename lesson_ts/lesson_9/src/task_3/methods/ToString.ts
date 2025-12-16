c35b80240b37286b7b64d0f3281bbfdb3922c1ea
Dedok8 <korobchenko08082005@gmail.com>
2025-12-16 19:15:43 +0200

    lesson_9

--- lesson_ts/lesson_9/src/task_3/methods/ToString.ts
+++
@@ -1,7 +0,0 @@
-// import ITransformer from "../ITransformer.js";
-
-// export default class ToString implements ITransformer<number, string> {
-//   transform(value: number): string {
-//     return String(value);
-//   }
-// }
